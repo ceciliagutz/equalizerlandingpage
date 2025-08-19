2acd23bc1498f8642fd781d69183c48eddc8b826
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-08-19 01:00:29 -0600

    "feat: Actualización de diseño responsivo"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,17 @@
 import './Footer.css';
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
 function Footer() {
     return (
         <footer className='footer'>
             <div className='footer-left'>
-                <p className='logo-footer'> equalizer </p>
+                <p className='logo-footer'>equalizer</p>
             </div>
             <div className='info-footer'>
-                <p className='derechos'> All rights reserved © Equalizer 2021</p>
-                <p className='contactus'> Have any problem? Contact us via social media or email us at</p>
-                <p className='correo'> equalizer@example.com</p>
-
+                <p className='derechos'>All rights reserved © Equalizer 2021</p>
+                <p className='contactus'>Have any problem? Contact us via social media or email us at</p>
+                <p className='correo'>equalizer@example.com</p>
             </div>
-
-
             <div className='footer-right'>
                 <div className='social-links'>
                     <a href='https://www.facebook.com' target="_blank" rel="noopener noreferrer">
@@ -27,10 +25,9 @@ function Footer() {
                     </a>
                 </div>
             </div>
-
         </footer>
     );
-
 }
 
 export default Footer;
+
