7d1f7ff579bb43c21a39cb0abb7bd56961ffec6e
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-08-18 23:27:09 -0600

    "feat: Estructura de landing"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      
-       <h1> Equalizer Landing page </h1>
+      <Header/>
+      <Hero/>
+      <Pricing/>
+      <Footer/>
+     
 
     </div>
   );
